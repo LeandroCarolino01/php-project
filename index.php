afb83d592b0e3d45762aa213515c43f3ef752f4b
Leandro.Carolino <carolino.leandro@gmail.com>
2019-12-26 23:20:26 +0100

    connected to the database

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-    
+    //connect to database
+    $conn = mysqli_connect('localhost', 'leandro', '123456', 'pizza');
+
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
