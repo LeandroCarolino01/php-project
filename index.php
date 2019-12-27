ed92a17ad624b0bbe39a7a7abeb05f72af30e570
Leandro.Carolino <carolino.leandro@gmail.com>
2019-12-27 15:59:23 +0100

    added queries to db

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?php
     //connect to database
     $conn = mysqli_connect('localhost', 'leandro', '123456', 'pizza');
-
+    //check connection
     if(!$conn){
         echo 'Connection error: ' . mysqli_connect_error();
     }
+
+    // write query for all pizza
+    $sql = 'SELECT title, ingredients, id FROM pizza';
+
+    // make query & get resuls
+    $result = mysqli_query($conn, $sql);
+
+    //fetch the resulting rows as an array
+    $pizza = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // free result from memory
+    mysqli_free_result($result);
+
+    // close connection
+    mysqli_close($conn);
+
+    print_r($pizza)
 ?>
 <!DOCTYPE html>
 <html lang="en">
