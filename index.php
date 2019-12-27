ecdf83ae956cc6d02deaf8d1afdfb5f2eeacf1f0
Leandro.Carolino <carolino.leandro@gmail.com>
2019-12-27 21:30:00 +0100

    data added to template

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     }
 
     // write query for all pizza
-    $sql = 'SELECT title, ingredients, id FROM pizza';
+    $sql = 'SELECT title, ingredients, id FROM pizza ORDER BY created_at';
 
     // make query & get resuls
     $result = mysqli_query($conn, $sql);
@@ -21,12 +21,31 @@
     // close connection
     mysqli_close($conn);
 
-    print_r($pizza)
+    // print_r($pizza)
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
 
+    <h4 class="center grey-text">Pizzas!</h4>
+        <div class="container">
+            <div class="row">
+                <?php foreach($pizza as $pizza){ ?>
+                    <div class="col s6 md3">
+                        <div class="card z-depth-0">
+                            <div class="card-content center">
+                                <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                                <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                            </div>
+                            <div class="card-action right-align">
+                                <a href="" class="brand-text">More Info</a>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php }?>
+            </div>
+        </div>
     <?php include('templates/footer.php'); ?>
     
 </body>
